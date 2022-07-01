cdf8679a111fc513aa1b712995535ff03ad61666
Andrew Gomez <drewgomez209@gmail.com>
2022-07-01 01:07:10 -0700

    profile card almost finished

--- src/components/ProfileCard/ProfileCard.jsx
+++ src/components/ProfileCard/ProfileCard.jsx
@@ -10,6 +10,27 @@ const ProfileCard = () => {
                 <img src={Cover} alt="" />
                 <img src={Profile} alt="" />
             </div>
+
+            <div className="ProfileName">
+                <span>Drew Gomez</span>
+                <span>Junior Full Stack Developer</span>
+            </div>
+
+            <div className="followStatus">
+                <hr />
+                <div>
+                    <div className="follow">
+                        <span>1</span>
+                        <span>Followings</span>
+                    </div>
+                    <div className="vl"></div>
+                    <div className="follow">
+                        <span>1</span>
+                        <span>Followers</span>
+                    </div>
+                </div>
+                <hr />
+            </div>
         </div>
     )
 }
--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import LogoSearch from '../LogoSearch/LogoSearch'
 import ProfileCard from '../ProfileCard/ProfileCard'
+import './Profile.css'
 
 const Profile = () => {
     return (
